a63d06b60d7a8d8737404d026aab616291b5adeb
kyle0527 <davincisnotebook001@gmail.com>
2025-10-31 07:37:23 -0700

    Add LIF and STDP neuron types with registry

--- src/bioneuronai/__init__.py
+++ src/bioneuronai/__init__.py
@@ -1,9 +1,21 @@
 from .core import BioNeuron, BioLayer, BioNet, cli_loop
+from .neuron_types import NEURON_REGISTRY, BaseSpikingNeuron, LIFNeuron, STDPNeuron
+
+__all__ = [
+    "BioNeuron",
+    "BioLayer",
+    "BioNet",
+    "cli_loop",
+    "BaseSpikingNeuron",
+    "LIFNeuron",
+    "STDPNeuron",
+    "NEURON_REGISTRY",
+]
 
 # 導入改進版本 (可選)
 try:
     from .improved_core import ImprovedBioNeuron, CuriositDrivenNet, BioNeuronV2
-    __all__ = ["BioNeuron", "BioLayer", "BioNet", "cli_loop", 
-               "ImprovedBioNeuron", "CuriositDrivenNet", "BioNeuronV2"]
 except ImportError:
-    __all__ = ["BioNeuron", "BioLayer", "BioNet", "cli_loop"]
+    pass
+else:
+    __all__.extend(["ImprovedBioNeuron", "CuriositDrivenNet", "BioNeuronV2"])
